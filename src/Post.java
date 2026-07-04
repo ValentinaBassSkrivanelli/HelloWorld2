1cc6ed8e921b52f157483b02d7d63bc72219d602
Valentina Bass Skrivanelli <aldanavalentinabass@gmail.com>
2026-07-04 11:27:12 -0300

    tested and working

--- src/Post.java
+++ src/Post.java
@@ -5,7 +5,7 @@ public class Post {
     private int id;
     private String content;
     private String author;
-    private LocaldateTime date;
+    private LocalDateTime date;
 
     public Post(int id, String content, String author) {
         /*Assigns the id passed into the constructor to the object's own id variable.*/
@@ -15,14 +15,14 @@ public class Post {
         /*Assigns the author passed into the constructor to the object's own author variable.*/
         this.author = author;
         /*Assigns the current date and time to the object's own date variable.*/
-        this.date = LocaldateTime.now();
+        this.date = LocalDateTime.now();
     }
 
     /*Creatr class: displayPost*/
 
     public void displayPost() {
-        System.out.printin("Post #" + id);
-        System.out.printin(author)
+        System.out.println("Post #" + id);
+        System.out.println(author);
         System.out.println(content);
         System.out.println(date);
     }
