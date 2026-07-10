d78e78789c4c85b696c05a02f8e01a36bc835e10
Valentina Bass Skrivanelli <aldanavalentinabass@gmail.com>
2026-07-10 17:39:55 -0300

    menu done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-public class Main {
-    //create a main method to test the User and Post classes
+import java.util.Scanner;
 
-    //create user object and post objects, add the posts to the user, and display the user's posts
+public class Main {
+
     public static void main(String[] args) {
-        //create user
+
+        // Create users
         User user1 = new User("Valentina", "valentina@example.com");
         User user2 = new User("John", "john@example.com");
 
-        //Create posts
+        // Create posts
         Post post1 = new Post(1, "Hello, this is my first post!", "Valentina");
         Post post2 = new Post(2, "Excited to share my thoughts with you all!", "Valentina");
         Post post3 = new Post(3, "What a beautiful day!", "John");
@@ -17,26 +18,67 @@ public class Main {
         user1.addPost(post2);
         user2.addPost(post3);
 
-        // Display users' posts
-        user1.showPosts();
-        user2.showPosts();
+        Scanner scanner = new Scanner(System.in);
 
-        // Test following and unfollowing
-        user1.follow(user2);
-        //UNCOMMENT TO TEST A SECOND TIME
-        //user1.unfollow(user2);
+        int option;
 
-        // TEST: Display followers and following
-        user1.showFollowers();
-        user1.showFollowing();
+        do {
 
+            System.out.println("\n===== BASIC SOCIAL MEDIA PLATFORM =====");
+            System.out.println("1. Show Valentina's posts");
+            System.out.println("2. Show John's posts");
+            System.out.println("3. Follow John");
+            System.out.println("4. Unfollow John");
+            System.out.println("5. Show feed");
+            System.out.println("6. Show following");
+            System.out.println("7. Show followers");
+            System.out.println("8. Exit");
+            System.out.print("Choose an option: ");
 
-        user1.showFeed();
+            option = scanner.nextInt();
 
-        user1.unfollow(user2);
+            switch (option) {
 
-        System.out.println();
+                case 1:
+                    user1.showPosts();
+                    break;
 
-        user1.showFollowing();
-}
+                case 2:
+                    user2.showPosts();
+                    break;
+
+                case 3:
+                    user1.follow(user2);
+                    System.out.println("Valentina is now following John.");
+                    break;
+
+                case 4:
+                    user1.unfollow(user2);
+                    System.out.println("Valentina unfollowed John.");
+                    break;
+
+                case 5:
+                    user1.showFeed();
+                    break;
+
+                case 6:
+                    user1.showFollowing();
+                    break;
+
+                case 7:
+                    user2.showFollowers();
+                    break;
+
+                case 8:
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+        } while (option != 8);
+
+        scanner.close();
+    }
 }
